31abdfbc57481753ebfc66fb4f0fad10347a5e20
zhengxiaojie <1430974500@qq.com>
2024-10-15 17:10:39 +0800

    fix:修改代码格式及添加毫米波雷达数据调试功能

--- include/ars_40X/ros/object_list_ros.hpp
+++ include/ars_40X/ros/object_list_ros.hpp
@@ -13,34 +13,37 @@
 #include "ars_40X/ars_40X_can.hpp"
 
 namespace ars_40X {
-class ObjectListROS {
- public:
-  ObjectListROS(ros::NodeHandle &nh, ARS_40X_CAN *continentalRadarCAN);
+    class ObjectListROS {
+    public:
+        ObjectListROS(ros::NodeHandle &nh, ARS_40X_CAN *continentalRadarCAN);
 
-  ~ObjectListROS();
+        ~ObjectListROS();
 
-  void set_frame_id(std::string frame_id);
+        void set_frame_id(std::string frame_id);
 
-  void send_object_0_status();
+        void send_object_0_status();
 
-  void send_object_1_general();
+        void send_object_1_general();
+
+        void pub_object_position();
 
 //  void send_object_2_quality();
 //
 //  void send_object_3_extended();
 
- private:
-  std::string frame_id_;
+    private:
+        std::string frame_id_;
 
-  ros::Publisher objects_data_pub_;
+        ros::Publisher objects_data_pub_;
+        ros::Publisher makerArrayPub_;
 
-  ARS_40X_CAN *ars_40X_can_;
+        ARS_40X_CAN *ars_40X_can_;
 
-  ObjectList object_list;
+        ObjectList object_list;
 
-  object_list::Object_0_Status *object_0_status_;
+        object_list::Object_0_Status *object_0_status_;
 
-  object_list::Object_1_General *object_1_general_;
+        object_list::Object_1_General *object_1_general_;
 
 //  object_list::Object_2_Quality *object_2_quality_;
 //
@@ -49,7 +52,7 @@ class ObjectListROS {
 //  int object_2_quality_id_;
 //
 //  int object_3_extended_id_;
-};
+    };
 }
 
 #endif //ARS_40X_OBJECT_LIST_ROS_HPP
--- src/ros/object_list_ros.cpp
+++ src/ros/object_list_ros.cpp
@@ -7,47 +7,113 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include "ars_40X/Object.h"
 #include "ars_40X/ObjectList.h"
+#include <visualization_msgs/MarkerArray.h>
 
 namespace ars_40X {
-ObjectListROS::ObjectListROS(ros::NodeHandle &nh, ARS_40X_CAN *ars_40X_can) :
-    ars_40X_can_(ars_40X_can) {
-  object_0_status_ = ars_40X_can_->get_object_0_status();
-  object_1_general_ = ars_40X_can_->get_object_1_general();
+    ObjectListROS::ObjectListROS(ros::NodeHandle &nh, ARS_40X_CAN *ars_40X_can) :
+            ars_40X_can_(ars_40X_can) {
+        object_0_status_ = ars_40X_can_->get_object_0_status();
+        object_1_general_ = ars_40X_can_->get_object_1_general();
 //  object_2_quality_ = ars_40X_can_->get_object_2_quality();
 //  object_3_extended_ = ars_40X_can_->get_object_3_extended();
-  objects_data_pub_ = nh.advertise<ObjectList>("ars_40X/objects", 10);
-}
+        objects_data_pub_ = nh.advertise<ObjectList>("ars_40X/objects", 10);
 
-ObjectListROS::~ObjectListROS() {
-}
+        makerArrayPub_=nh.advertise<visualization_msgs::MarkerArray >("visualization_marker_distination",5);
+    }
 
-void ObjectListROS::set_frame_id(std::string frame_id) {
-  frame_id_ = frame_id;
-}
+    ObjectListROS::~ObjectListROS() {
+    }
 
-void ObjectListROS::send_object_0_status() {
-  object_list.header.stamp = ros::Time::now();
-  object_list.header.frame_id = frame_id_;
-  object_list.objects.erase(
-      object_list.objects.begin() ,
-      object_list.objects.begin() + object_list.objects.size());
-  objects_data_pub_.publish(object_list);
-  object_list.objects.clear();
+    void ObjectListROS::set_frame_id(std::string frame_id) {
+        frame_id_ = frame_id;
+    }
+
+    void ObjectListROS::pub_object_position() {
+        visualization_msgs::MarkerArray marker_array;
+
+        for(const auto& object:object_list.objects){
+            visualization_msgs::Marker marker;
+
+            marker.header.frame_id=frame_id_;
+            marker.header.stamp=ros::Time::now();
+            marker.type=visualization_msgs::Marker::CUBE;
+            marker.id=object.id;
+            //设置物体大小
+            marker.scale.x=0.25;
+            marker.scale.y=0.25;
+            marker.scale.z=0.25;
+            //设置物体颜色
+            marker.color.r=2;
+            marker.color.g=0;
+            marker.color.b=0;
+            marker.color.a=1;
+
+            marker.pose.position.x=object.position.pose.position.x;
+            marker.pose.position.y=object.position.pose.position.y;
+            marker.pose.position.z=0.25;
+            marker.lifetime=ros::Duration(0.01);
+
+            marker_array.markers.push_back(marker);
+
+            ///文字数组
+            visualization_msgs::Marker marker2;
+            marker2.action=visualization_msgs::Marker::ADD;
+            marker2.header.frame_id=frame_id_;
+            marker2.header.stamp=ros::Time::now();
+            marker2.id=object.id+100;
+            marker2.type=visualization_msgs::Marker::TEXT_VIEW_FACING;
+
+            marker2.scale.x=0.5;
+            marker2.scale.y=0.5;
+            marker2.scale.z=0.5;
+
+            marker2.color.r=2;
+            marker2.color.g=0;
+            marker2.color.b=0;
+            marker2.color.a=0.4;
+
+            marker2.pose.position.x=object.position.pose.position.x;
+            marker2.pose.position.y=object.position.pose.position.y;
+            marker2.pose.position.z=0.75;
+            marker2.lifetime=ros::Duration(0.01);
+
+            std::ostringstream str;
+            str << "Radar " << "x: " << object.position.pose.position.x << " y:" << object.position.pose.position.y;
+            marker2.text=str.str();
+            marker_array.markers.push_back(marker2);
+
+        }
+
+        makerArrayPub_.publish(marker_array);
+
+    }
+
+    void ObjectListROS::send_object_0_status() {
+        object_list.header.stamp = ros::Time::now();
+        object_list.header.frame_id = frame_id_;
+//  object_list.objects.erase(
+//      object_list.objects.begin() ,
+//      object_list.objects.begin() + object_list.objects.size());
+
+        pub_object_position();///调试
+
+        objects_data_pub_.publish(object_list);
+        object_list.objects.clear();
 //  object_2_quality_id_ = 0;
 //  object_3_extended_id_ = 0;
-}
+    }
 
-void ObjectListROS::send_object_1_general() {
-  Object object;
-  object.id = object_1_general_->get_object_id();
-  object.position.pose.position.x = object_1_general_->get_object_long_dist();
-  object.position.pose.position.y = object_1_general_->get_object_lat_dist();
-  object.relative_velocity.twist.linear.x = object_1_general_->get_object_long_rel_vel();
-  object.relative_velocity.twist.linear.y = object_1_general_->get_object_lat_rel_vel();
-  object.dynamic_property = object_1_general_->get_object_dyn_prop();
-  object.rcs = object_1_general_->get_object_rcs();
-  object_list.objects.push_back(object);
-}
+    void ObjectListROS::send_object_1_general() {
+        Object object;
+        object.id = object_1_general_->get_object_id();
+        object.position.pose.position.x = object_1_general_->get_object_long_dist();
+        object.position.pose.position.y = object_1_general_->get_object_lat_dist();
+        object.relative_velocity.twist.linear.x = object_1_general_->get_object_long_rel_vel();
+        object.relative_velocity.twist.linear.y = object_1_general_->get_object_lat_rel_vel();
+        object.dynamic_property = object_1_general_->get_object_dyn_prop();
+        object.rcs = object_1_general_->get_object_rcs();
+        object_list.objects.push_back(object);
+    }
 
 //void ObjectListROS::send_object_2_quality() {
 //  object_list.objects[object_2_quality_id_].position.covariance[0] =
